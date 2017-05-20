659a0c28ecf2f0bf43635fc9c899366b9a9ddfed
felichio <fsafaridis@hotmail.com>
2017-05-20 15:15:06 +0300

    Database model change, test update

--- Model/test.php
+++ Model/test.php
@@ -3,7 +3,7 @@
     require("admin.php");
     require("doctor.php");
     require("patient.php");
-    require("Drug.php");
+    require("drug.php");
 
     $admin = new Admin(1, "felichio", "felix", "saf", "example@example.com", "asdasf");
     $doc = new Doctor(1, "felichio", "felix", "saf", "example@example.com", "asdasf", "892345784235");
