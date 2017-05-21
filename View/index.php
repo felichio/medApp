55bf07d05fbf40fea8e3ddd39e1e36893cca3b92
felichio <fsafaridis@hotmail.com>
2017-05-21 16:13:42 +0300

    index.php create, bootstrap add

---
+++ View/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<hmtl>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-witdh, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous">
+    </head>
+
+    <body>
+        <h1>WELCOME TO MEDWORLD</h1>
+    </body>
+
+</html>
