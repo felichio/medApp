d8763190a1af67bd22d83f2d2bb8de1d2884eb8b
felichio <fsafaridis@hotmail.com>
2017-05-21 23:55:58 +0300

    functions.php changing require_once calls

--- View/functions/functions.php
+++ View/functions/functions.php
@@ -1,6 +1,17 @@
 <?php
 
-    function getUserFromDb($mysqli, $username, $password, $table) {
+    require_once(__DIR__ . "/../../Model/admin.php");
+    require_once(__DIR__ . "/../../Model/doctor.php");
+    require_once(__DIR__ . "/../../Model/drug.php");
+    require_once(__DIR__ . "/../../Model/patient.php");
+    require_once(__DIR__ . "/connection.php");
+
+
+
+    function getUserFromDb($username, $password, $table) {
+
+        global $mysqli;
+
         $query = "select * from $table where username = ? and password = SHA1(?)";
         $stmt = $mysqli->prepare($query);
         $stmt->bind_param("ss", $username, $password);
@@ -9,7 +20,11 @@
 
         $res = $stmt->get_result();
 
-        
+        if ($row = $res->fetch_assoc()) {
+            return $table = "Admin" ? new Admin($row["id"], $row["username"], $row["firstname"], $row["lastname"], $row["email"], $row["password"]) :
+                                      new Doctor();
+        }
+
     }
 
 
