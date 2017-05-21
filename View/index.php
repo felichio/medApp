fe4eabf3cf5bf45acaad4d412edec0b7684bc8d6
felichio <fsafaridis@hotmail.com>
2017-05-21 18:04:29 +0300

    add jquery lib, statistics view, form folder, css for header and index page

--- View/index.php
+++ View/index.php
@@ -9,10 +9,71 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
         crossorigin="anonymous">
+
+        <link rel="stylesheet" href="css/header.css" type="text/css">
+        <link rel="stylesheet" href="css/index.css" type="text/css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
 
     <body>
-        <h1>WELCOME TO MEDWORLD</h1>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">MedWorld</a>
+                </div>
+                <p class="navbar-text navbar-right" id="signin-label">
+                    <a href="#">Sign-In</a>
+                    <a href="#">Register</a>
+                </p>
+            </div>
+
+        </nav>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Weclome to MedWorld</h1>
+                <p>A simple web application for prescription handling</p>
+            </div>
+
+
+            <div class="stats">
+                <fieldset>
+                    <legend>Statistics</legend>
+                    Total number of Doctors registered: <span class="stats-num">0</span><br>
+                    Total number of Patients treated: <span class="stats-num">0</span><br>
+                    Total number of Prescriptions issued: <span class="stats-num">0</span><br>
+                    Total number of Drugs registered: <span class="stats-num">0</span><br>
+                </fieldset>
+            </div>
+
+            <div class="form-input">
+
+            </div>
+
+        </div>
+
+
+        <!-- <div class="container">
+            <div class="page-header">
+                <h1>Weclome to MedWorld</h1><br>
+                <h3>A simple web application for prescription handling </h3>
+            </div>
+
+            <div class="stats">
+                <fieldset>
+                    <legend>Statistics</legend>
+                    Total number of Doctors registered
+                </fieldset>
+            </div>
+        </div> -->
+
+
+
+
+
+
+
+
     </body>
 
 </html>
