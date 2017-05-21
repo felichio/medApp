b01c8a50e1c66651adffca3bef7b3bc5ceb0dba8
felichio <fsafaridis@hotmail.com>
2017-05-21 23:56:51 +0300

    login.php add if..elseif

--- View/login.php
+++ View/login.php
@@ -1,14 +1,22 @@
 <?php
 
 
-    require_once("functions/connection.php");
+
     require_once("functions/functions.php");
 
     $username = $_POST["username"];
     $password = $_POST["password"];
 
 
-    getUserFromDb($mysqli, $username, $password, "Admin");
+    $admin = getUserFromDb($username, $password, "Admin");
+    $doctor = getUserFromDb($username, $password, "Doctor");
 
+    if (empty($admin) && empty($doctor)) {
+        echo "Empty";
+    } else if (!empty($admin)) {
+        echo "Admin " . $admin . " detected";
+    } else if (!empty($doctor)) {
+        echo "Doctor " . $doctor . " detected";
+    }
 
  ?>
