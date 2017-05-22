f494b3f220da12f77de1c68572a09ff4bc23fb0a
felichio <fsafaridis@hotmail.com>
2017-05-22 21:22:01 +0300

    admin and doctor php now redirect to index in case of missing authentication token

--- View/admin.php
+++ View/admin.php
@@ -4,10 +4,13 @@
 
     session_start();
 
-
-    $title = "Admin";
-    $cssName = "admin";
-    render("views/admin_landing.php", ["title" => $title, "cssName" => $cssName]);
+    if (isAuthenticated() && getLoggedUser() instanceof Admin) {
+        $title = "Admin";
+        $cssName = "admin";
+        render("views/admin_landing.php", ["title" => $title, "cssName" => $cssName]);
+    } else {
+        redirect("View/index.php");
+    }
 
 
 
--- View/doctor.php
+++ View/doctor.php
@@ -4,11 +4,13 @@
 
     session_start();
 
-
-    $title = "Doctor";
-    $cssName = "doctor";
-    render("views/doctor_landing.php", ["title" => $title, "cssName" => $cssName]);
-
+    if (isAuthenticated() && getLoggedUser() instanceof Doctor) {
+        $title = "Doctor";
+        $cssName = "doctor";
+        render("views/doctor_landing.php", ["title" => $title, "cssName" => $cssName]);
+    } else {
+        redirect("View/index.php");
+    }
 
 
  ?>
