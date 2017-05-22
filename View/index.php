8336854372920c5796b936692b32f9f86c855c77
felichio <fsafaridis@hotmail.com>
2017-05-22 16:28:29 +0300

    index.php now calls render() pointing to landing.php

--- View/index.php
+++ View/index.php
@@ -1,37 +1,14 @@
 <?php
 
+
+    require_once("functions/functions.php");
+
+
     $title = "MedWorld";
     $cssName = "index";
-
-    include("partials/header.php");
-
-?>
+    $jsName = "index";
 
 
-        <div class="container">
-            <div class="jumbotron">
-                <h1>Weclome to MedWorld</h1>
-                <p>A simple web application for prescription handling</p>
-            </div>
-
-
-            <div class="stats">
-                <fieldset>
-                    <legend>Statistics</legend>
-                    Total number of Doctors registered: <span class="stats-num">0</span><br>
-                    Total number of Patients treated: <span class="stats-num">0</span><br>
-                    Total number of Prescriptions issued: <span class="stats-num">0</span><br>
-                    Total number of Drugs registered: <span class="stats-num">0</span><br>
-                </fieldset>
-            </div>
-
-            <div class="form-input">
-            </div>
-
-        </div>
-
-<?php
-
-    include("partials/footer.php");
+    render("views/landing.php", ["title" => $title, "cssName" => $cssName, "jsName" => $jsName]);
 
 ?>
---
+++ View/views/landing.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="jumbotron">
+        <h1>Weclome to MedWorld</h1>
+        <p>A simple web application for prescription handling</p>
+    </div>
+
+
+    <div class="stats">
+        <fieldset>
+            <legend>Statistics</legend>
+            Total number of Doctors registered: <span class="stats-num">0</span><br>
+            Total number of Patients treated: <span class="stats-num">0</span><br>
+            Total number of Prescriptions issued: <span class="stats-num">0</span><br>
+            Total number of Drugs registered: <span class="stats-num">0</span><br>
+        </fieldset>
+    </div>
+
+    <div class="form-input">
+    </div>
+
+</div>
