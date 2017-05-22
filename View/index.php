4bd7c18b058d2958d8336fa7ae1aa83bda2fb259
felichio <fsafaridis@hotmail.com>
2017-05-22 18:01:14 +0300

    index.php now uses sessions and authentication function

--- View/index.php
+++ View/index.php
@@ -1,14 +1,26 @@
 <?php
 
-
+    session_start();
     require_once("functions/functions.php");
 
 
-    $title = "MedWorld";
-    $cssName = "index";
-    $jsName = "index";
 
 
-    render("views/landing.php", ["title" => $title, "cssName" => $cssName, "jsName" => $jsName]);
+    if (!isAuthenticated()) {
+        $title = "MedWorld";
+        $cssName = "index";
+        $jsName = "index";
+        render("views/landing.php", ["title" => $title, "cssName" => $cssName, "jsName" => $jsName]);
+    } else {
+        $user = $_SESSION["user"];
+
+        if ($user instanceof Admin) {
+            
+        } else {
+
+        }
+    }
+
+
 
 ?>
