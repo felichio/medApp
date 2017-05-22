012233c3468f8abef137137e1d3db0e9361c0b10
felichio <fsafaridis@hotmail.com>
2017-05-22 19:19:50 +0300

    admin and doctor sample pages created

---
+++ View/views/admin_page.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="jumbotron">
+        <h1>Administrator</h1>
+        <h2>sample</h2>
+    </div>
+</div>
---
+++ View/views/doctor_page.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="jumbotron">
+        <h1>Doctor</h1>
+        <h2>sample</h2>
+    </div>
+</div>
