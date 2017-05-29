18b8ed1e617d08008851ab40975e45da26964322
felichio <fsafaridis@hotmail.com>
2017-05-29 21:52:17 +0300

    add drug_edt case for drug handling

--- View/adminonfire.php
+++ View/adminonfire.php
@@ -8,17 +8,32 @@
         if (isset($_GET["doctor_edt"])) {
             $id = $_GET["doctor_edt"];
             $doctor = getDoctorById($id);
+            if ($doctor instanceof Doctor) {
+                $title = "Edit";
+                $cssName = "admin-doc-edit";
+                $jsName = "admin-doc-edit";
 
-            $title = "Edit";
-            $cssName = "admin-doc-edit";
-            $jsName = "admin-doc-edit";
+                render("views/admin/doctor_edit.php", ["title" => $title, "cssName" => $cssName, "jsName" => $jsName, "doctor" => $doctor]);
+            } else {
+                redirect("View/admin.php");
+            }
 
-            render("views/admin/doctor_edit.php", ["title" => $title, "cssName" => $cssName, "jsName" => $jsName, "doctor" => $doctor]);
 
         } else if (isset($_GET["doctor_del"])) {
             $id = $_GET["doctor_del"];
-            echo "del";
-            print_r($id);
+            $doctor = getDoctorById($id);
+
+            if ($doctor instanceof Doctor) {
+                deleteDoctorById($id);
+                $successes = ["Dr " . $doctor->getLastname() . " deleted successfully"];
+                $_SESSION["successes"] = $successes;
+            }
+
+            redirect("View/admin.php");
+        } else if (isset($_GET["drug_edt"])) {
+            $code = $_GET["drug_edt"];
+            $drug = getDrugByCode($code);
+            echo $drug;
         }
 
 
