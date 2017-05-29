6268d3a418620ba0e8986d42fc0ba7c22676086a
felichio <fsafaridis@hotmail.com>
2017-05-29 21:52:46 +0300

    doctor.php rendering path changed

--- View/doctor.php
+++ View/doctor.php
@@ -7,7 +7,7 @@
     if (isAuthenticated() && getLoggedUser() instanceof Doctor) {
         $title = "Doctor";
         $cssName = "doctor";
-        render("views/doctor_landing.php", ["title" => $title, "cssName" => $cssName]);
+        render("views/doctor/doctor_landing.php", ["title" => $title, "cssName" => $cssName]);
     } else {
         redirect("View/index.php");
     }
